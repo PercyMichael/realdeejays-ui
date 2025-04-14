fc58381c332732dddd9b168cc980225964c3d1e7
user <user@Digital-Carfts-Man.local>
2025-04-14 13:01:22 +0300

    fix search icon

--- src/components/LeftSidebar.jsx
+++ src/components/LeftSidebar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { FaFacebook, FaTwitter, FaInstagram } from "react-icons/fa";
 import { FaHeart } from "react-icons/fa6";
-import { GoHeart, GoHomeFill } from "react-icons/go";
+import { GoHeart, GoHomeFill, GoSearch } from "react-icons/go";
 
 import { IoHeadsetOutline } from "react-icons/io5";
 import { PiMusicNoteDuotone } from "react-icons/pi";
@@ -12,25 +12,11 @@ const LeftSidebar = () => {
     <div className="flex flex-col px-5 py-5 gap-10 w-1/6 h-screen">
       {/* search */}
       <div className="px-3 flex items-center border border-white/10 bg-black font-light text-sm rounded-full">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke-width="1.5"
-          stroke="currentColor"
-          className="size-6"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
-          />
-        </svg>
-
+        <GoSearch size={20} className="min-w-[25px] min-h-[20px]" />
         <input
           type="text"
           placeholder="Search"
-          className="p-2 rounded  text-white outline-none bg-transparent"
+          className="p-2 rounded text-white outline-none bg-transparent"
           onChange={(e) => console.log(e.target.value)}
         />
       </div>
