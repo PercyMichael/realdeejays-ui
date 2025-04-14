fa02993b39aed9cd22a7a0d5313edfcd0053e9dd
user <user@Digital-Carfts-Man.local>
2025-04-14 12:46:17 +0300

    try

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
+
+window.addEventListener("load", () => {
+  const preloader = document.getElementById("preloader");
+  const root = document.getElementById("root");
+  if (preloader && root) {
+    preloader.style.display = "none";
+    root.style.display = "block";
+    document.body.style.backgroundImage = "url('./public/bg.webp')";
+  }
+});
