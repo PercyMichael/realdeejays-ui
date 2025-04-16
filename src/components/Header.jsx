aed5e94529b26d7bcd507c5b4e4544b0d8b353e3
user <user@Digital-Carfts-Man.local>
2025-04-16 15:53:17 +0300

    top border thicker

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 const Header = ({ isScrolled }) => {
   return (
     <div
-      className={`flex items-center justify-between w-full px-5 md:px-10 py-4 border-x border-white/5 sticky top-0 z-10 ${
+      className={`flex items-center justify-between w-full px-5 md:px-10 py-4 border-x border-white/10 md:border-white/5 sticky top-0 z-10 ${
         isScrolled ? "backdrop-blur-lg bg-red-900/10" : "bg-transparent"
       }`}
     >
